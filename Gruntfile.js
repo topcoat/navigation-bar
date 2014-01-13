ee6f4a3ae93744a23eb16afce7441476effd5349
garthdb <garthdb@gmail.com>
2014-01-13 15:06:39 -0800

    removed fonts folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,46 @@
 /**
- *
- * Copyright 2012 Adobe Systems Inc.;
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+*
+* Copyright 2012 Adobe Systems Inc.;
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
 /*global module:false*/
 
 module.exports = function(grunt) {
 
-    // Project configuration.
     grunt.initConfig({
 
         clean: {
-            release: ['css']
+            release: ['css'],
         },
 
-        stylus: {
-            mobilelight: {
-                options: {
-                    paths: ['node_modules/topcoat-navigation-bar-base/src', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src', 'node_modules/topcoat-theme/src/includes'],
-                    import: ['theme-topcoat-mobile-light', 'layout', 'position'],
-                    compress: false
-                },
-
-                files: [{
-                    src: 'src/topcoat-navigation-bar.styl',
-                    dest: 'css/topcoat-navigation-bar-mobile-light.css'
-                }]
+        topcoat: {
+            options: {
+                browsers: ['last 2 versions'],
+                namespace: 'topcoat',
+                license: grunt.file.read('test/fixtures/license.txt')
             },
-
-            mobiledark: {
-                options: {
-                    paths: ['node_modules/topcoat-navigation-bar-base/src', 'node_modules/topcoat-utils/src/mixins', 'node_modules/topcoat-theme/src', 'node_modules/topcoat-theme/src/includes'],
-                    import: ['theme-topcoat-mobile-dark', 'layout', 'position'],
-                    compress: false
-                },
-
+            compile: {
                 files: [{
-                    src: 'src/topcoat-navigation-bar.styl',
-                    dest: 'css/topcoat-navigation-bar-mobile-dark.css'
-                }]
-            }
-
-        },
-
-        cssmin: {
-            minify: {
-                expand: true,
-                cwd: 'css',
-                src: ['*.css', '!*.min.css'],
-                dest: 'css',
-                ext: '.min.css'
+                        expand: true,
+                        cwd: 'test/fixtures',
+                        src: ['*.css'],
+                        dest: 'css/',
+                        ext: '.css'
+                    }
+                ]
             }
         },
 
@@ -81,31 +59,46 @@ module.exports = function(grunt) {
             }
         },
 
+        autoprefixer: {
+          dist: {
+            files: [{
+              expand: true,
+              cwd: 'css',
+              src: ['*.css', '!*.min.css'],
+              dest: 'css/'
+            }]
+          }
+        },
+
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'release/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'release/css/',
+                ext: '.min.css'
+            }
+        },
+
         simplemocha: {
             all: {
                 src: ['test/*.test.js']
             }
-        },
-
-        watch: {
-            files: 'src/**/*.styl',
-            tasks: ['build', 'test']
         }
 
     });
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-topcoat');
     grunt.loadNpmTasks('grunt-topdoc');
 
-    // Default task.
-    grunt.registerTask('default', ['clean', 'build', 'test', 'release']);
-    grunt.registerTask('build', ['stylus']);
+    grunt.registerTask('default', ['clean', 'build', 'test','release']);
+    grunt.registerTask('build', ['topcoat']);
     grunt.registerTask('test', ['simplemocha']);
     grunt.registerTask('release', ['cssmin', 'topdoc']);
 
 };
+
